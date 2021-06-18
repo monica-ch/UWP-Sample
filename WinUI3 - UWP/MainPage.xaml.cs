54a43076b47e37aaba336044bf7277caa3599caa
Monica Chintala <monicach@microsoft.com>
2021-06-17 17:00:28 -0700

    add uwp sample code to repo

---
+++ WinUI3 - UWP/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Microsoft.Web.WebView2.Core;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WinUI3___UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            MyWebView.NavigationStarting += Navigation_Starting;
+            MyWebView.NavigationCompleted += Navigation_Completed;
+        }
+
+        private void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Uri targetUri = new Uri(addressBar.Text);
+                MyWebView.Source = targetUri;
+            }
+            catch (FormatException ex)
+            {
+            }
+        }
+
+        private void Navigation_Starting(WebView2 sender, CoreWebView2NavigationStartingEventArgs args)
+        {
+            String uri = args.Uri;
+        }
+        private void Navigation_Completed(WebView2 sender, CoreWebView2NavigationCompletedEventArgs args)
+        {
+            bool status = args.IsSuccess;
+            var id = args.NavigationId;
+        }
+    }
+}
